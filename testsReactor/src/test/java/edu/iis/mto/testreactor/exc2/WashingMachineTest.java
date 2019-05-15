c7342fb539f320d4d2153aebc8ae31edb8dae7eb
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-05-15 17:45:40 +0200

    initialize method added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -1,11 +1,29 @@
 package edu.iis.mto.testreactor.exc2;
 
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WashingMachineTest {
+
+    private Program program;
+    private WashingMachine washingMachine;
+    DirtDetector dirtDetector;
+    Engine engine;
+    WaterPump waterPump;
+
+    @Before
+    public void intialize() {
+        this.dirtDetector = mock(DirtDetector.class);
+        this.engine = mock(Engine.class);
+        this.waterPump = mock(WaterPump.class);
+
+        this.washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
+
+    }
 
     @Test
     public void itCompiles() {
